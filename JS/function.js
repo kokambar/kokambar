9f2d38c4d30c6a625bdfcf8d8c7d410a18e9d80d
kokin <kokambar@gmail.com>
2023-02-27 21:26:55 -0300

    first commit

---
+++ JS/function.js
@@ -0,0 +1,41 @@
+
+ document.getElementById("boton_Experiencia_laboral").onclick = function () {
+   
+   
+   
+    document.getElementById('seccion4').style.display='block';
+    document.getElementById('seccion1').style.display='none';
+    document.getElementById('seccion2').style.display='none';
+    document.getElementById('seccion3').style.display='none';
+   
+}
+document.getElementById("boton_Presentacion").onclick = function () {
+   
+   
+   
+   document.getElementById('seccion1').style.display='block';
+   document.getElementById('seccion2').style.display='none';
+   document.getElementById('seccion3').style.display='none';
+   document.getElementById('seccion4').style.display='none';
+  
+}
+document.getElementById("boton_Contacto").onclick = function () {
+   
+   
+   
+   document.getElementById('seccion2').style.display='block';
+   document.getElementById('seccion1').style.display='none';
+   document.getElementById('seccion3').style.display='none';
+   document.getElementById('seccion4').style.display='none';
+  
+}
+document.getElementById("boton_Estudios").onclick = function () {
+   
+   
+   
+   document.getElementById('seccion3').style.display='block';
+   document.getElementById('seccion1').style.display='none';
+   document.getElementById('seccion2').style.display='none';
+   document.getElementById('seccion4').style.display='none';
+  
+}
